f5e9cf5b52cff525699cfc4cd5d0c4284b55c2a1
diglopes <diego-chez@hotmail.com>
2019-12-12 13:48:14 -0300

    ensure AuthUseCase calls LoadUserByEmailRepo with correct email

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,6 +1,10 @@
 const { MissingParamError } = require('../../utils/errors')
 
 class AuthUseCase {
+  constructor (loadUserByEmailRepo) {
+    this.loadUserByEmailRepo = loadUserByEmailRepo
+  }
+
   async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
@@ -8,6 +12,8 @@ class AuthUseCase {
     if (!password) {
       throw new MissingParamError('password')
     }
+
+    await this.loadUserByEmailRepo.load(email)
   }
 }
 
@@ -22,5 +28,18 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCase()
     const promise = sut.auth('any_email@email.com')
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  test('Should call LoadUserByEmailRepo withcorrect email', async () => {
+    class LoadUserByEmailRepoSpy {
+      async load (email) {
+        this.email = email
+      }
+    }
+
+    const loadUserByEmailRepoSpy = new LoadUserByEmailRepoSpy()
+    const sut = new AuthUseCase(loadUserByEmailRepoSpy)
+    await sut.auth('any_email@email.com', 'any_password')
+    expect(loadUserByEmailRepoSpy.email).toBe('any_email@email.com')
   })
 })
