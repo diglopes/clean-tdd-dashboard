f4d391b5cf22c4bf3dbcc4c0a3372384d3f94053
diglopes <diego-chez@hotmail.com>
2019-12-14 13:14:09 -0300

    move sut creation to a factory helper method

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,21 +1,24 @@
 const jwt = require('jsonwebtoken')
 
-class TokenGenerator {
-  async generate (id) {
-    return jwt.sign(id, 'secret')
+const makeSut = () => {
+  class TokenGenerator {
+    async generate (id) {
+      return jwt.sign(id, 'secret')
+    }
   }
+  return new TokenGenerator()
 }
 
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', async () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     jwt.token = null
     const token = await sut.generate('any_id')
     expect(token).toBeNull()
   })
 
   test('Should return a token if JWT returns token', async () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
   })
