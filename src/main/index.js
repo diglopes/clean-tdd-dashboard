9b790bbf0b390888c7f7ad1c5df0466cdd2d2501
diglopes <diego-chez@hotmail.com>
2019-12-22 18:36:35 -0300

    add helmet

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const helmet = require('helmet')
 const app = express()
+
+app.use(helmet())
+app.get('/api-mango', (req, res) => {
+  res.send('mango')
+})
 
 app.listen(3333, () => console.log('>> Server running'))
