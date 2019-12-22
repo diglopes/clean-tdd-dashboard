8e6b98f9cc6548376ab811a63446154c002be2e2
diglopes <diego-chez@hotmail.com>
2019-12-22 19:35:42 -0300

    ensure requests parse body as JSON

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,6 +1,9 @@
 const helmet = require('helmet')
 const cors = require('../middlewares/cors')
+const jsonParser = require('../middlewares/json-parser')
+
 module.exports = app => {
   app.use(helmet())
   app.use(cors)
+  app.use(jsonParser)
 }
--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -19,4 +19,14 @@ describe('App Setup', () => {
     expect(res.headers['access-control-allow-methods']).toBe('*')
     expect(res.headers['access-control-allow-headers']).toBe('*')
   })
+
+  test('Should parse body as JSON', async () => {
+    app.post('/test_json_parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post('/test_json_parser').send({ working: true })
+      .expect({ working: true })
+  })
 })
