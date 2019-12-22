90622419e2e00b15a532e32660ad27b765b512ab
diglopes <diego-chez@hotmail.com>
2019-12-22 19:20:55 -0300

    enable cors

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -2,4 +2,10 @@ const helmet = require('helmet')
 
 module.exports = app => {
   app.use(helmet())
+  app.use((req, res, next) => {
+    res.set('access-control-allow-origin', '*')
+    res.set('access-control-allow-methods', '*')
+    res.set('access-control-allow-headers', '*')
+    next()
+  })
 }
