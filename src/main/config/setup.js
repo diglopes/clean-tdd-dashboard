454d788c6f6d292a2c45538a8faaee8d99200a16
diglopes <diego-chez@hotmail.com>
2019-12-22 19:37:28 -0300

    move helmet to middleware folder

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,9 +1,9 @@
-const helmet = require('helmet')
+const helmet = require('../middlewares/helmet')
 const cors = require('../middlewares/cors')
 const jsonParser = require('../middlewares/json-parser')
 
 module.exports = app => {
-  app.use(helmet())
+  app.use(helmet)
   app.use(cors)
   app.use(jsonParser)
 }
