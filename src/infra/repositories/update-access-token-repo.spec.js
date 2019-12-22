01bc14268c2776d2b3696d1e45eec76d107b0ae5
diglopes <diego-chez@hotmail.com>
2019-12-22 17:17:03 -0300

    ensure UpdateAccessTokenRepo updates user with the given accessToken

---
+++ src/infra/repositories/update-access-token-repo.spec.js
@@ -0,0 +1,46 @@
+const MongoHelper = require('../helpers/mongo-helper')
+const UserSchema = require('./schemas/user-schema')
+
+class UpdateAccessTokenRepo {
+  constructor (userSchema) {
+    this.userSchema = userSchema
+  }
+
+  async update (userId, accessToken) {
+    await this.userSchema.updateOne({
+      _id: userId
+    }, {
+      $set: {
+        accessToken
+      }
+    })
+  }
+}
+
+describe('UpdateAccessToken Repository', () => {
+  beforeAll(async () => {
+    const uri = 'mongodb://localhost:27017/test'
+    await MongoHelper.connect(uri)
+  })
+
+  beforeEach(async () => {
+    await UserSchema.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should update the user with the given accessToken', async () => {
+    const fakeUser = await UserSchema.create({
+      email: 'valid_email@email.com',
+      password: 'hashed_password',
+      name: 'João'
+    })
+
+    const sut = new UpdateAccessTokenRepo(UserSchema)
+    await sut.update(fakeUser._id, 'valid_token')
+    const updatedFakeUser = await UserSchema.findById(fakeUser._id)
+    expect(updatedFakeUser.accessToken).toBe('valid_token')
+  })
+})
