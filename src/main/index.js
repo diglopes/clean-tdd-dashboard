fcf544fdc66c9d2ba7112430e90dc78f72fa6ef4
diglopes <diego-chez@hotmail.com>
2019-12-23 12:33:46 -0300

    connect to mongodb before listen for app

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,16 @@
 const app = require('./config/app')
+const MongoHelper = require('../infra/helpers/mongo-helper')
+const env = require('./config/env')
 
-app.listen(3333, () => console.log('>> Server running'))
+const bootstrap = async () => {
+  try {
+    await MongoHelper.connect(env.mongoUrl)
+    console.log('>> Database connected')
+
+    await app.listen(3333, () => console.log('>> Server running'))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+bootstrap()
