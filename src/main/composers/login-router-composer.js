3d90588d5ac2daac959580d8e0871cc7beb74c94
diglopes <diego-chez@hotmail.com>
2019-12-23 15:21:42 -0300

    move loginRouter composition to its own file

---
+++ src/main/composers/login-router-composer.js
@@ -0,0 +1,24 @@
+const LoginRouter = require('../../presentation/routers/login-router')
+const AuthUseCase = require('../../domain/usecases/auth-usecase')
+const EmailValidator = require('../../utils/helpers/email-validator')
+const LoadUserByEmailRepo = require('../../infra/repositories/load-user-by-email-repo')
+const UpdateAccessTokenRepo = require('../../infra/repositories/update-access-token-repo')
+const Encrypter = require('../../utils/helpers/encrypter')
+const TokenGenerator = require('../../utils/helpers/token-generator')
+const env = require('../config/env')
+const userSchema = require('../../infra/repositories/schemas/user-schema')
+
+const tokenGenerator = new TokenGenerator(env.tokenSecret)
+const encrypter = new Encrypter()
+const loadUserByEmailRepo = new LoadUserByEmailRepo(userSchema)
+const updateAccessTokenRepo = new UpdateAccessTokenRepo(userSchema)
+const emailValidator = new EmailValidator()
+const authUseCase = new AuthUseCase({
+  loadUserByEmailRepo,
+  updateAccessTokenRepo,
+  encrypter,
+  tokenGenerator
+})
+const loginRouter = new LoginRouter({ authUseCase, emailValidator })
+
+module.exports = loginRouter
--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/test'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/test',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret'
 }
